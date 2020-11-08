008294668e0be4c62e51db4a3577c085e4bcb429
exequii <71356016+exequii@users.noreply.github.com>
2020-11-08 19:03:57 -0300

    Cargo 2 test y dejo incompleto el import total.

--- EDA2 EZE/test/eda/TestResto.java
+++ EDA2 EZE/test/eda/TestResto.java
@@ -114,5 +114,73 @@ public class TestResto {
 
 	}
 	
+	@Test
+	public void agregarComidayBebidaAlPedido() {
+		Restaurant resto1 = new Restaurant("Antares");
+		Mesa mesa1 = new Mesa(4,001);
+		resto1.agregarMesa(mesa1);
+		Mozo juan = new Mozo("juan", "Gonzales", (long) 123, 123);
+		resto1.agregarMozo(juan);
+		Pedido nuevo = new Pedido(1, mesa1, juan);
+		resto1.agregarPedido(nuevo);
+		Alimento cerveza = new Bebida("cerveza", 150.0);
+		Alimento pizza = new Bebida("pizza", 200.0);
+		
+		resto1.agregarComidaALaCarta(cerveza);
+		resto1.agregarComidaALaCarta(pizza);
+		
+		assertTrue(nuevo.agregarAlimentoAlPedido(cerveza, resto1));
+		assertTrue(nuevo.agregarAlimentoAlPedido(pizza, resto1));
+
+	}
+	
+	@Test
+	public void queNoPuedaAgregarAlimentoAlPedido() {
+		Restaurant resto1 = new Restaurant("Antares");
+		Mesa mesa1 = new Mesa(4,001);
+		resto1.agregarMesa(mesa1);
+		Mozo juan = new Mozo("juan", "Gonzales", (long) 123, 123);
+		resto1.agregarMozo(juan);
+		Pedido nuevo = new Pedido(1, mesa1, juan);
+		resto1.agregarPedido(nuevo);
+		Alimento cerveza = new Bebida("cerveza", 150.0);
+		Alimento pizza = new Bebida("pizza", 200.0);
+		
+		resto1.agregarComidaALaCarta(cerveza);
+		
+		assertTrue(nuevo.agregarAlimentoAlPedido(cerveza, resto1));
+		assertFalse(nuevo.agregarAlimentoAlPedido(pizza, resto1)); // ESTE DARIA FALSO PORQUE NO LO ENCUENTRA EN CARTA YA QUE NO LO AGREGAMOS.
+
+	}
+	
+	@Test
+	public void importeTotalPedido() {
+		Restaurant resto1 = new Restaurant("Antares");
+		Mesa mesa1 = new Mesa(4,001);
+		resto1.agregarMesa(mesa1);
+		Mozo juan = new Mozo("juan", "Gonzales", (long) 123, 123);
+		resto1.agregarMozo(juan);
+		Pedido nuevo = new Pedido(1, mesa1, juan);
+		resto1.agregarPedido(nuevo);
+		
+		Alimento cerveza = new Bebida("cerveza", 150.0);
+		Alimento pizza = new Comida("pizza", 200.0);
+		
+		resto1.agregarComidaALaCarta(cerveza);
+		resto1.agregarComidaALaCarta(pizza);
+		nuevo.agregarAlimentoAlPedido(cerveza, resto1);
+		nuevo.agregarAlimentoAlPedido(pizza, resto1);
+		
+		Integer ve=350;;
+		Integer importeTotal=0;
+		
+		for(Pedido nuevo : alimentosEnElPedido) {
+			if(nuevo.getNumeroDePedido()==1) {
+			importeTotal+=nuevo.getAlimentosEnElPedido().getPrecio();
+		}
+		}
+		assertEquals(ve,importeTotal,0.0);
+	
+	}
 	
 }
